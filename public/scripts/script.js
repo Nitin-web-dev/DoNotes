57074741213afbbbe878a8118437da7f58362e42
Nitin-web-dev <nitinwebdev5@gmail.com>
2026-08-30 14:35:48 +0530

    added time and date in both pages

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,22 +1,37 @@
 const checkbox = document.querySelectorAll('input[type="checkbox"]');
+const timeAndDate = document.querySelector(".timeAndDate");
+
+function settimeandDate() {
+  const now = new Date();
+  const dateTime = now.toLocaleString([], {
+    month: "numeric",
+    day: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+  });
+  timeAndDate.textContent = dateTime;
+}
+settimeandDate();
+
+setInterval(settimeandDate(), 1000);
 
 checkbox.forEach((checkbox) => {
   checkbox.addEventListener("change", async function () {
     let id = this.dataset.id;
     let completed = this.checked;
     // console.log(id, completed)
-    try{
-
-        const result = await fetch(`/todo/${id}`, {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ completed }),
-        });
-        // console.log('fetch after', result)
-    }catch(error){
-        console.error('error message', error.message)
+    try {
+      const result = await fetch(`/todo/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ completed }),
+      });
+      // console.log('fetch after', result)
+    } catch (error) {
+      console.error("error message", error.message);
     }
   });
 });
